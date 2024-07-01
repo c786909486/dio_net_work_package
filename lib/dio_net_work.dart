a11852c501f694940c4f3cb4df178ac74d16a21f
ckz <18869978285@163.com>
2024-07-01 15:52:09 +0800

    增加dio初始化恢复options

--- lib/dio_net_work.dart
+++ lib/dio_net_work.dart
@@ -3,3 +3,4 @@ export 'package:dio_net_work/app_download_dialog.dart';
 export 'package:dio_net_work/app_download_utils.dart';
 export 'package:dio_net_work/http_utils.dart';
 export 'net_ext_utils.dart';
+export 'package:dio/dio.dart';
